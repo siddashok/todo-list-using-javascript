d439c2b5d6d41698d993090f1f7a0883d981cc51
Siddharth Ashok <siddharthashok@Siddharths-MacBook-Pro.local>
2019-01-05 17:07:12 -0500

    first commit

---
+++ js/sidd.js
@@ -0,0 +1,93 @@
+
+window.onload = function() {
+	var form = document.getElementById("form");
+	var input = document.getElementById("input");
+	var btn = document.getElementById("btn");
+	var list = document.getElementById("list");	
+	var btnClr = document.getElementById("btnClr");	
+	var id = 1;
+	var liItem = "";
+	var todoList = [];
+
+	//button event listener
+	btn.addEventListener("click", addTodoItem);
+
+	//list event listener
+	list.addEventListener("click", boxChecked);
+
+	//event listener for clear list
+	btnClr.addEventListener("click", clearList);
+
+	if(localStorage.length <= 0) {
+		btnClr.style.display = "none"; 
+		console.log("button");
+	}
+
+	//checking localstorage has data
+	if(localStorage.length > 0) {
+		displayList();
+	}
+
+
+	//add todo item to list
+	function addTodoItem() {
+		if(input.value === "") {
+			alert("You must enter some value!");
+		}
+		else {
+			if(list.style.borderTop === "") {
+				console.log("here!")
+				list.style.borderTop = "2px solid white";
+				btnClr.style.display = "inline";
+			}
+			var text = input.value;	
+			var item = `<li id="li-${id}">${text}<input id="box-${id}" 	class="checkboxes" type="checkbox"></li>`;				
+			list.insertAdjacentHTML('beforeend', item);	
+			liItem = {item: text, checked: false};
+			todoList.push(liItem);		
+			id++;
+			addToLocalStorage()
+			form.reset();
+		}
+	}
+
+	//adding string through style to list itme
+	function boxChecked(event) {
+		const element = event.target;
+		if(element.type === "checkbox") {
+			element.parentNode.style.textDecoration = "line-through";
+			todoList = JSON.parse(localStorage.getItem("todoList"));
+			todoList[element.id.split('-')[1]-1].checked = element.checked.toString();
+			localStorage.setItem("todoList", JSON.stringify(todoList));
+		}
+	}
+
+	
+
+	//display all todo list
+	function displayList() {
+		list.style.borderTop = "2px solid white";
+		todoList = JSON.parse(localStorage.getItem("todoList"));
+		todoList.forEach(function(element) {
+			console.log(element.item)
+			var text = element.item;
+			var item = `<li id="li-${id}">${text}<input id="box-${id}" class="checkboxes" type="checkbox"></li>`;
+			list.insertAdjacentHTML("beforeend", item);
+			if(element.checked) {
+				var li = document.getElementById("li-"+id);
+				li.style.textDecoration = "line-through";
+				li.childNodes[1].checked = element.checked;
+			}
+			id++;
+		});
+	}
+
+	//clear list event listener
+	function clearList() {
+		todoList = [];
+		localStorage.clear();
+		list.innerHTML = "";
+		btnClr.style.display = "none";
+		list.style.borderTop = "";
+	}
+}
